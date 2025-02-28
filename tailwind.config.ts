33a3084b5afabd08c5511662ddb6c85fb020e681
Faizyab Khalid <129151546+Faiz510@users.noreply.github.com>
2025-02-28 12:16:34 +0500

    Initial Commit
    - Initial Project setup
    - Next and tailwind install

---
+++ tailwind.config.ts
@@ -0,0 +1,20 @@
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './app/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#FEC60D',
+        secondary: '#242C4F',
+      },
+    },
+  },
+  plugins: [],
+};
+
+export default config;
